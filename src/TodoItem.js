4cc06711f7a0f6442eaf1df061b649454a6383ca
Dmitriy <dexan.abriel@gmail.com>
2019-09-16 16:01:51 +0300

    test

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,28 @@
-import React from "react"
+/**
+ * Challenge: Style the completed todo items differently from the incomplete items.
+ */
+
+import React from "react";
 
 function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input 
-                type="checkbox" 
-                checked={props.item.completed} 
-                onChange={() => props.handleChange(props.item.id)}
-            />
-            <p>{props.item.text}</p>
-        </div>
-    )
+  const completedStyle = {
+    fontstyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through"
+  };
+
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p style={props.item.completed ? completedStyle : null}>
+        {props.item.text}
+      </p>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
